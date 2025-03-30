5a8b371982c0c0119c91ed8996a883e57d3cae31
ksrisumit <ksrisumit@pop-os.localdomain>
2025-03-30 13:14:08 +0530

    Learnt Middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello From Backend");
+
+const { userAuth, adminAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.post("/user/login", (req, res) => {
+  res.send("User Logged In successfully");
+});
+
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User Data Send Successfully");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent Successfully");
 });
 
 app.listen(3000, () => {
