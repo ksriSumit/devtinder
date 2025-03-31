443f3d9f38b368df8ddb9b18b21c3f53959985e4
ksrisumit <ksrisumit@pop-os.localdomain>
2025-03-31 09:45:31 +0530

    Learned User Schema & Models

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
 
-const { userAuth, adminAuth } = require("./middlewares/auth");
+const connectDB = require("./config/database");
 
-app.use("/admin", adminAuth);
+app.use(express.json());
 
-app.post("/user/login", (req, res) => {
-  res.send("User Logged In successfully");
+app.get("/", (req, res) => {
+  res.send("Hello From Backend");
 });
 
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("User Data Send Successfully");
+app.use("/", (req, res, next) => {
+  console.log("Working till here");
+  next();
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data Sent Successfully");
+app.post("/signup", (req, res, next) => {
+  console.log(req.params);
+  res.status(201).send("User Created Successfully");
 });
 
-app.listen(3000, () => {
-  console.log("App listening on port 3000!");
-});
+//Start the DB first, then only start the application
+connectDB()
+  .then(() => {
+    console.log("Connection is Established to Database");
+    app.listen(3000, () => {
+      console.log("App listening on port 3000!");
+    });
+  })
+  .catch((error) => {
+    console.error("Error Occurred:\n", error.message);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  return await mongoose.connect(
+    "mongodb+srv://ksrisumit:4jH27viSjjZ8ab86@sumitbackend.v5zme.mongodb.net/"
+  );
+};
+
+module.exports = connectDB;
