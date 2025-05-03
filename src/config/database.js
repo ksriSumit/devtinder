90497c2236577560dd93a38f760dbf3e51934e2c
ksrisumit <ksrisumit@pop-os.localdomain>
2025-05-03 15:26:50 +0530

    added databse info in .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbString = process.env.DB_CONNECTION_STRING;
 
 const connectDB = async () => {
-  return await mongoose.connect(
-    "mongodb+srv://ksrisumit:4jH27viSjjZ8ab86@sumitbackend.v5zme.mongodb.net/devTinder"
-  );
+  return await mongoose.connect(dbString);
 };
 
 module.exports = connectDB;
